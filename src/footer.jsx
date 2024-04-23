8aadcbc55032cd540ce09d1088c43dd83382529a
Anonscodex <normalstuff2001@gmail.com>
2024-04-23 08:56:55 -0700

    made small changes

--- src/footer.jsx
+++ src/footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
     return (
-        <div className="flex justify-center items-center h-18 bg-black sm:justify-center">
+        <div className="flex fixed bottom-0 w-full z-50 justify-center items-center h-18 bg-black sm:justify-center">
             <h3 className="text-white my-5 px-8 font-sans font-bold">DevMeme &copy; 2024</h3>
             
         </div>
